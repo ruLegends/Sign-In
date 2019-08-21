e245a981f9379f6528e6da3e75d3ec88ccab3baa
ruLegends <ruLegens@gmail.com>
2019-08-21 19:23:01 +0300

    New

--- SignIn.py
+++ SignIn.py
@@ -1,25 +1,42 @@
 from tkinter import *
 import os
+import argparse
+from argparse import ArgumentParser
+from pathlib import *
 
 
-loginpassfile = r"loginpassfile.txt"  # txt file with login and password
-inputfile = r"pathfile.txt"  # file with paths to your programs
+parser = argparse.ArgumentParser()  # txt file with login and password
+parser.add_argument(
+        "--path",
+        type=lambda p: Path(p).absolute(),
+        default=Path(__file__).absolute().parent / "loginpassfile.txt",
+        help="Path to the pass file",
+    )
+parser.add_argument(
+        "--prog",
+        type=lambda p: Path(p).absolute(),
+        default=Path(__file__).absolute().parent / "pathfile.txt",
+        help="Path to the path file",
+    )
+allowed_credentials = parser.parse_args()
+scenarions = parser.parse_args()
+
 
 
 def on_closing():  # function for frame update
     app.update()
 
 
-def password_check(*args):  # function for login and password check
+def password_check():  # function for login and password check
     readlog = login.get()  # reads login from EntryBox
     readpass=password.get()  # reads password from EntryBox
-    fi = open(loginpassfile)  # open txt file
+    fi = open(allowed_credentials.path)  # open txt file
     for line in fi:  # reads file by line
         log = (line[:line.find(":")].strip())  # take login from file(first part (log)) log : pass
         pas = (line[line.find(":") + 1: ].strip())  # take password from file(second part (pass)) log : pass
         if readpass==pas and readlog == log:  # password and login check
             app.destroy()  # closes frame
-            f = open(inputfile)  # open file with any paths
+            f = open(scenarions.prog)  # open file with any paths
             for line in f:
                 os.startfile(line.strip())  # starts something that you specified in the file
             f.close()  # closes file
